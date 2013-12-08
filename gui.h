ce86cc3345d52a61e4b477b7e729523d3aa1079c
Konstantin Zauber <tesla.rus@gmail.com>
2013-12-08 19:32:53 +0400

    Speed field are moved from character structure to the entity structure. TR4+ ceiling climb fixes (in TR4+ climbable sector info uses only in start climbing positions).

---
+++ gui.h
@@ -0,0 +1,193 @@
+
+#ifndef ENGINE_GUI_H
+#define ENGINE_GUI_H
+
+
+typedef struct gui_text_line_s
+{
+    char                       *text;
+    uint16_t                    buf_size;
+    int8_t                      show_rect;
+    int8_t                      show;
+
+    GLint                       x;
+    GLint                       y;
+
+    GLfloat                     rect_color[4];
+    GLfloat                     font_color[4];
+    GLfloat                     rect_border;
+    GLfloat                     rect[4];                                        //x0, yo, x1, y1
+
+    struct gui_text_line_s     *next;
+    struct gui_text_line_s     *prev;
+} gui_text_line_t, *gui_text_line_p;
+
+// Immutable bars enumeration.
+// These are the bars that are always exist in GUI.
+// Scripted bars could be created and drawn separately later.
+enum Bars
+{
+    BAR_HEALTH,     // TR 1-5
+    BAR_AIR,        // TR 1-5, alternate state - gas (TR5)
+    BAR_SPRINT,     // TR 3-5
+    BAR_FREEZE,     // TR 3 only
+    BAR_LASTINDEX
+};
+
+// Bar color types.
+// Each bar part basically has two colours - main and fade.
+enum BarColorType
+{
+    BASE_MAIN,
+    BASE_FADE,
+    ALT_MAIN,
+    ALT_FADE,
+    BACK_MAIN,
+    BACK_FADE,
+    BORDER_MAIN,
+    BORDER_FADE
+};
+
+// Main bar class.
+class ProgressBar
+{
+public:
+    ProgressBar();  // Bar constructor.
+
+    void Show(float value);    // Main show bar procedure.
+
+    void SetColor(BarColorType colType, uint8_t R, uint8_t G, uint8_t B, uint8_t alpha);
+    void SetDimensions(float X, float Y, float width, float height, float borderSize);
+    void SetValues(float maxValue, float warnValue);
+    void SetBlink(int interval);
+    void SetExtrude(bool enabled, uint8_t depth);
+    void SetAutoshow(bool enabled, int delay, bool fade, int fadeDelay);
+
+    bool          Visible;              // Is it visible or not.
+    bool          Alternate;            // Alternate state, in which bar changes color to AltColor.
+
+    bool          Invert;               // Invert decrease direction flag.
+    bool          Vertical;             // Change bar style to vertical.
+
+private:
+
+    bool          UpdateResolution      // Try to update bar resolution.
+                       (int ScrWidth,
+                        int ScrHeight);
+
+    void          RecalculateSize();    // Recalculate size and position.
+    void          RecalculatePosition();
+
+    float         mX;                   // Real X position.
+    float         mY;                   // Real Y position.
+    float         mWidth;               // Real width.
+    float         mHeight;              // Real height.
+    float         mBorderWidth;         // Real border size (horizontal).
+    float         mBorderHeight;        // Real border size (vertical).
+
+    float         mAbsX;                // Absolute (resolution-independent) X position.
+    float         mAbsY;                // Absolute Y position.
+    float         mAbsWidth;            // Absolute width.
+    float         mAbsHeight;           // Absolute height.
+    float         mAbsBorderSize;       // Absolute border size (horizontal).
+
+    float         mBaseMainColor[5];    // Color at the min. of bar.
+    float         mBaseFadeColor[5];    // Color at the max. of bar.
+    float         mAltMainColor[5];     // Alternate main color.
+    float         mAltFadeColor[5];     // Alternate fade color.
+    float         mBackMainColor[5];    // Background main color.
+    float         mBackFadeColor[5];    // Background fade color.
+    float         mBorderMainColor[5];  // Border main color.
+    float         mBorderFadeColor[5];  // Border fade color.
+
+    int8_t        mBaseBlendingMode;    // Blending modes for all bar parts.
+    int8_t        mBackBlendingMode;    // Note there is no alt. blending mode, cause
+    int8_t        mBorderBlendingMode;  // base and alt are actually the same part.
+
+    bool          mExtrude;             // Extrude effect.
+    float         mExtrudeDepth[5];     // Extrude effect depth.
+
+    float         mMaxValue;            // Maximum possible value.
+    float         mWarnValue;           // Warning value, at which bar begins to blink.
+    float         mLastValue;           // Last value back-up for autoshow on change event.
+
+    bool          mBlink;               // Warning state (blink) flag.
+    float         mBlinkInterval;       // Blink interval (speed).
+    float         mBlinkCnt;            // Blink counter.
+
+    bool          mAutoShow;            // Autoshow on change flag.
+    float         mAutoShowDelay;       // How long bar will stay on-screen in AutoShow mode.
+    float         mAutoShowCnt;         // Auto-show counter.
+    bool          mAutoShowFade;        // Fade flag.
+    float         mAutoShowFadeDelay;   // Fade length.
+    float         mAutoShowFadeCnt;     // Fade progress counter.
+
+    float         mRangeUnit;           // Range unit used to set base bar size.
+    float         mBaseSize;            // Base bar size.
+    float         mBaseRatio;           // Max. / actual value ratio.
+
+    int           mLastScrWidth;        // Back-up to check resolution change.
+    int           mLastScrHeight;
+};
+
+void Gui_Init();
+void Gui_Destroy();
+
+void Gui_AddLine(gui_text_line_p line);
+void Gui_DeleteLine(gui_text_line_p line);
+void Gui_RenderStrings();
+void Gui_RenderStringsRect();
+/*
+ * Calculates rect coordinates around the text
+ */
+gui_text_line_p Gui_StringAutoRect(gui_text_line_p l);
+/**
+ * Draws text using a current console font.
+ */
+gui_text_line_p Gui_OutTextXY(int x, int y, const char *fmt, ...);
+
+/**
+ * Helper method to setup OpenGL state for console drawing.
+ *
+ * Either changes to 2D matrix state (is_gui = 1) or away from it (is_gui = 0). Does not do any drawing.
+ */
+void Gui_SwitchConGLMode(char is_gui);
+
+/**
+ * Draws wireframe of this frustum.
+ *
+ * Expected state:
+ *  - Vertex array is enabled, color, tex coord, normal disabled
+ *  - No vertex buffer object is bound
+ *  - Texturing is disabled
+ *  - Alpha test is disabled
+ *  - Blending is enabled
+ *  - Lighting is disabled
+ * Ignored state:
+ *  - Currently bound texture.
+ *  - Currently bound element buffer.
+ *  - Depth test enabled (disables it, then restores)
+ *  - Vertex pointer (changes it)
+ *  - Matrices (changes them, restores)
+ *  - Line width (changes it, then restores)
+ *  - Current color (changes it)
+ * Changed state:
+ *  - Current position will be arbitrary.
+ *  - Vertex pointer will be arbitray.
+ *  - Current color will be arbitray (set by console)
+ *  - Blend mode will be SRC_ALPHA, ONE_MINUS_SRC_ALPHA (set by console)
+ */
+void Gui_Render();
+/**
+*  Draw simple rectangle.
+*  Only state it changes is the blend mode, according to blendMode value.
+*/
+
+void Gui_DrawRect(const GLfloat &x, const GLfloat &y,
+                  const GLfloat &width, const GLfloat &height,
+                  const GLfloat colorUpperLeft[], const GLfloat colorUpperRight[],
+                  const GLfloat colorLowerLeft[], const GLfloat colorLowerRight[],
+                  const int &blendMode);
+void Gui_DrawCrosshair();
+void Gui_DrawBars();
+#endif
---
+++ redblack.cpp
@@ -0,0 +1,561 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "redblack.h"
+#include "system.h"
+
+void findHeight(RedBlackHeader_p header, RedBlackNode_p t, int curr);
+void deleteFixup(RedBlackHeader_p header, RedBlackNode_p x, char x_null);
+
+// This method search the tree for a node with key 'key', and
+// return the node on success otherwise treeNULL.
+RedBlackNode_p RB_SearchNode(void *data, RedBlackHeader_p header)
+{
+    RedBlackNode_p current = header->root;
+    
+    while(current != NULL)
+    {
+        if(header->rb_compEQ(data, current->data))                                                       //data1 == data2
+        {
+            return (current);
+        }
+        current = (header->rb_compLT(data, current->data)) ? current->left : current->right;           //data1 < data2
+    }
+    return NULL;
+}
+
+// This is a recursive method that find a node height.
+void findHeight(RedBlackHeader_p header, RedBlackNode_p t, int curr)
+{
+    if(header->height < curr)
+    {
+        header->height = curr;
+    }
+    if(t->left != NULL)
+    {
+        findHeight(header, t->left, curr+1);
+    }
+    if(t->right != NULL)
+    {
+        findHeight(header, t->right, curr+1);
+    }
+}
+
+
+// This method update the tree height it uses a recursive method
+// findheight.
+void updateHeight(RedBlackHeader_p header)
+{
+    header->height = 0;
+    if (header->root != NULL)
+    {
+        findHeight(header, header->root, 1);
+    }
+}
+
+
+void rotateLeft(RedBlackHeader_p header, RedBlackNode_p x)
+{
+
+   /**************************
+    *  rotate node x to left *
+    **************************/
+    RedBlackNode_p y = x->right;
+
+    /* establish x->right link */
+    x->right = y->left;
+    if (y->left != NULL)
+    {
+        y->left->parent = x;
+    }
+    
+    /* establish y->parent link */
+    if(y != NULL)
+    {
+        y->parent = x->parent;
+    }
+    
+    if(x->parent)
+    {
+        if(x == x->parent->left)
+        {
+            x->parent->left = y;
+        }
+        else
+        {
+            x->parent->right = y;
+        }
+    } 
+    else
+    {
+        header->root = y;
+    }
+
+    /* link x and y */
+    y->left = x;
+    if(x != NULL)
+    {
+        x->parent = y;
+    }
+}
+
+void rotateRight(RedBlackHeader_p header, RedBlackNode_p x)
+{
+
+   /****************************
+    *  rotate node x to right  *
+    ****************************/
+
+    RedBlackNode_p y = x->left;
+
+    /* establish x->left link */
+    x->left = y->right;
+    if(y->right != NULL)
+    {
+        y->right->parent = x;
+    }
+
+    /* establish y->parent link */
+    if(y != NULL)
+    {
+        y->parent = x->parent;
+    }
+
+    if(x->parent)
+    {
+        if(x == x->parent->right)
+        {
+            x->parent->right = y;
+        }
+        else
+        {
+            x->parent->left = y;
+        }
+    } 
+    else
+    {
+        header->root = y;
+    }
+
+    /* link x and y */
+    y->right = x;
+    if (x != NULL) x->parent = y;
+}
+
+RedBlackHeader_p RB_Init()
+{
+    RedBlackHeader_p p = (RedBlackHeader_p)malloc(sizeof(struct RedBlackHeader_s));
+    if(p == NULL)
+    {
+        return NULL;
+    }
+
+    p->rb_compEQ = NULL;
+    p->rb_compLT = NULL;
+    p->rb_free_data = NULL;
+    p->root = NULL;
+    p->height = 0;
+    p->node_count = 0;
+    
+    return p;
+}
+
+void RB_Free(RedBlackHeader_p p)
+{
+    RB_MakeEmpty(p);
+    free(p);
+}
+
+void RB_EmptyTree(RedBlackNode_p n, RedBlackHeader_p p)
+{
+    if(n->left != NULL)
+    {
+        RB_EmptyTree(n->left, p);
+    }
+    if(n->right != NULL)
+    {
+        RB_EmptyTree(n->right, p);
+    }
+    if(p->rb_free_data)
+    {
+        p->rb_free_data(n->data);
+    }
+    p->node_count--;
+}
+
+void RB_MakeEmpty(RedBlackHeader_p header)
+{
+    if(header && header->root)
+    {
+        RB_EmptyTree(header->root, header);
+    }
+}
+
+void insertFixup(RedBlackHeader_p p, RedBlackNode_p x)
+{
+
+   /*************************************
+    *  maintain Red-Black tree balance  *
+    *  after inserting node x           *
+    *************************************/
+
+    /* check Red-Black properties */
+    while (x != p->root && x->parent != NULL && x->parent->color == RBC_RED)
+    {
+        /* we have a violation */
+        if (x->parent == x->parent->parent->left)
+        {
+            RedBlackNode_p y = x->parent->parent->right;
+            if (y != NULL && y->color == RBC_RED)
+            {
+                /* uncle is Red */
+                x->parent->color = RBC_BLACK;
+                y->color = RBC_BLACK;
+                x->parent->parent->color = RBC_RED;
+                x = x->parent->parent;
+            } 
+            else
+            {
+
+                /* uncle is Black */
+                if (x == x->parent->right)
+                {
+                    /* make x a left child */
+                    x = x->parent;
+                    rotateLeft(p, x);                          //rotateLeft(x);
+                }
+
+                /* recolor and rotate */
+                x->parent->color = RBC_BLACK;
+                x->parent->parent->color = RBC_RED;
+                rotateRight(p, x->parent->parent);                              //rotateRight(x->parent->parent)
+            }
+        } 
+        else
+        {
+            /* mirror image of above code */
+            RedBlackNode_p y = x->parent->parent->left;
+            if (y != NULL && y->color == RBC_RED)
+            {
+                /* uncle is Red */
+                x->parent->color = RBC_BLACK;
+                y->color = RBC_BLACK;
+                x->parent->parent->color = RBC_RED;
+                x = x->parent->parent;
+            } 
+            else
+            {
+                /* uncle is Black */
+                if (x == x->parent->left)
+                {
+                    x = x->parent;
+                    rotateRight(p, x);                                 //rotateRight(x);
+                }
+                x->parent->color = RBC_BLACK;
+                x->parent->parent->color = RBC_RED;
+                rotateLeft(p, x->parent->parent);                               //rotateLeft(x->parent->parent);
+            }
+        }
+    }
+    p->root->color = RBC_BLACK;
+}
+
+RedBlackNode_p RB_InsertIgnore(void *data, RedBlackHeader_p header)
+{
+    RedBlackNode_p current, parent, x;
+
+   /***********************************************
+    *  allocate node for data and insert in tree  *
+    ***********************************************/
+
+    /* find where node belongs */
+    current = header->root;
+    parent = NULL;
+    while(current != NULL)
+    {
+        if (header->rb_compEQ(data, current->data))
+        {
+            return NULL;
+        }
+        parent = current;
+        current = (header->rb_compLT(data, current->data)) ? current->left : current->right;
+    }
+
+    /* setup new node */
+    x = (RedBlackNode_p)malloc(sizeof(struct RedBlackNode_s));
+    if(x == NULL)
+    {
+        Sys_extError("insufficient memory (insertNode)\n");
+    }
+    x->data = data;
+    x->parent = parent;
+    x->left = NULL;
+    x->right = NULL;
+    x->color = RBC_RED;                                                         // new node is red
+
+    /* insert node in tree */
+    if(parent)
+    {
+        if(header->rb_compLT(data, parent->data))
+        {
+            parent->left = x;
+        }
+        else
+        {
+            parent->right = x;
+        }
+    } 
+    else
+    {
+        header->root = x;
+    }
+
+    insertFixup(header, x);
+    header->node_count ++;
+    return(x);
+}
+
+RedBlackNode_p RB_InsertReplace(void *data, RedBlackHeader_p header)
+{
+    RedBlackNode_p current, parent, x;
+
+   /***********************************************
+    *  allocate node for data and insert in tree  *
+    ***********************************************/
+
+    /* find where node belongs */
+    current = header->root;
+    parent = NULL;
+    while(current != NULL)
+    {
+        if(header->rb_compEQ(data, current->data))
+        {
+            void *old_data = current->data;
+            current->data = data;
+            if(header->rb_free_data)
+            {
+                header->rb_free_data(old_data);
+            }
+            return NULL;
+        }
+        parent = current;
+        current = (header->rb_compLT(data, current->data)) ? current->left : current->right;
+    }
+
+    /* setup new node */
+    x = (RedBlackNode_p)malloc(sizeof(struct RedBlackNode_s));
+    if(x == NULL)
+    {
+        Sys_extError("insufficient memory (insertNode)\n");
+    }
+    x->data = data;
+    x->parent = parent;
+    x->left = NULL;
+    x->right = NULL;
+    x->color = RBC_RED;                                                         // new node is red
+
+    /* insert node in tree */
+    if(parent)
+    {
+        if(header->rb_compLT(data, parent->data))
+        {
+            parent->left = x;
+        }
+        else
+        {
+            parent->right = x;
+        }
+    } 
+    else
+    {
+        header->root = x;
+        //return x;
+    }
+
+    insertFixup(header, x);
+    header->node_count ++;
+    return(x);
+}
+
+
+void deleteFixup(RedBlackHeader_p header, RedBlackNode_p x, char x_null)
+{
+
+   /*************************************
+    *  maintain Red-Black tree balance  *
+    *  after deleting node x            *
+    *************************************/
+
+    while(x != header->root && x->color == RBC_BLACK)
+    {
+        if(x->parent->left == (x_null ? NULL : x))
+        {
+            RedBlackNode_p w = x->parent->right;
+            if(w->color == RBC_RED)
+            {
+                w->color = RBC_BLACK;
+                x->parent->color = RBC_RED;
+                rotateLeft (header, x->parent);
+                w = x->parent->right;
+            }
+            if((w->right == NULL || w->right->color == RBC_BLACK) &&
+               (w->left == NULL || w->left->color == RBC_BLACK))
+            {
+                w->color = RBC_RED;
+                x = x->parent;
+            } 
+            else
+            {
+                if(w->right == NULL || w->right->color == RBC_BLACK)
+                {
+                    w->left->color = RBC_BLACK;
+                    w->color = RBC_RED;
+                    rotateRight (header, w);
+                    w = x->parent->right;
+                }
+                w->color = x->parent->color;
+                x->parent->color = RBC_BLACK;
+                w->right->color = RBC_BLACK;
+                rotateLeft (header, x->parent);
+                x = header->root;
+            }
+        } 
+        else
+        {
+            RedBlackNode_p w = x->parent->left;
+            if (w->color == RBC_RED)
+            {
+                w->color = RBC_BLACK;
+                x->parent->color = RBC_RED;
+                rotateRight (header, x->parent);
+                w = x->parent->left;
+            }
+            if ((w->right == NULL || w->right->color == RBC_BLACK) &&
+                (w->left == NULL || w->left->color == RBC_BLACK))
+            {
+                w->color = RBC_RED;
+                x = x->parent;
+            } 
+            else
+            {
+                if (w->left == NULL || w->left->color == RBC_BLACK)
+                {
+                    w->right->color = RBC_BLACK;
+                    w->color = RBC_RED;
+                    rotateLeft (header, w);
+                    w = x->parent->left;
+                }
+                w->color = x->parent->color;
+                x->parent->color = RBC_BLACK;
+                w->left->color = RBC_BLACK;
+                rotateRight (header, x->parent);
+                x = header->root;
+            }
+        }
+        x_null = 0;
+    }
+    x->color = RBC_BLACK;
+}
+
+
+void RB_Delete(RedBlackHeader_p header, RedBlackNode_p z)
+{
+    RedBlackNode_p x, y, w;
+    void *data = NULL;
+    char x_null = 0;
+   /*****************************
+    *  delete node z from tree  *
+    *****************************/
+
+    if (z == NULL)
+    {
+        return;
+    }
+
+    data = z->data;
+
+    if(header->node_count == 1 && z == header->root)
+    {
+        header->node_count --;
+        if(header->rb_free_data)
+        {
+            header->rb_free_data(data);
+        }
+        header->root = NULL;
+        return;
+    }
+
+    if (z->left == NULL || z->right == NULL)
+    {
+        /* y has a NULL node as a child */
+        y = z;
+    } 
+    else
+    {
+        /* find tree successor with a NULL node as a child */
+        y = z->right;
+        while (y->left != NULL) y = y->left;
+    }
+
+    /* x is y's only child */
+    if(y->left != NULL)
+    {
+        x = y->left;
+    }
+    else
+    {
+        x = y->right;
+    }
+    
+    /* remove y from the parent chain */
+
+    if(y->parent)
+    {
+        if(y == y->parent->left)
+        {
+            y->parent->left = x;
+            w = y->parent->right;
+        }
+        else
+        {
+            y->parent->right = x;
+            w = y->parent->left;
+        }
+    }
+    else
+    {
+        header->root = x;
+    }
+
+    if(x == NULL)
+    {
+        if(w != NULL)
+        {
+            x = y;
+            x_null = 1;
+        }
+        else
+        {
+            x = y->parent;
+        }
+    }
+    else
+    {
+        x->parent = y->parent;
+    }
+    
+    if(y != z)
+    {
+        z->data = y->data;
+    }
+
+    if(y->color == RBC_BLACK)
+    {
+        deleteFixup(header, x, x_null);
+    }
+
+    header->node_count--;
+    if(header->rb_free_data)
+    {
+        header->rb_free_data(data);
+    }
+}
