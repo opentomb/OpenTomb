38388d5595eff42b94dd01dcd5b30ba79427a320
Konstantin Zauber <tesla.rus@gmail.com>
2014-05-20 19:31:04 +0400

    Fixed LUA text out to theconsole.

--- lua/luaconf.h
+++ lua/luaconf.h
@@ -211,8 +211,8 @@
 */
 #if defined(LUA_LIB) || defined(lua_c)
 #include <stdio.h>
-#define luai_writestring(s,l)	fwrite((s), sizeof(char), (l), stdout)
-#define luai_writeline()	(luai_writestring("\n", 1), fflush(stdout))
+//#define luai_writestring(s,l)	fwrite((s), sizeof(char), (l), stdout)
+//#define luai_writeline()	(luai_writestring("\n", 1), fflush(stdout))
 #endif
 
 /*
