b9fbfb0f2a0d098778c45a6a32a51da2d9a8851a
gh0stblade <misternightmare@live.com >
2014-05-28 18:28:17 +0100

    Updated opcode info, changed all opcode names to match those from the original GAMEFLOW.EXE added missing ones + info about them!

--- gameflow.h
+++ gameflow.h
@@ -7,39 +7,39 @@ typedef struct gameflow_manager_s
     const char* Script;
     const char* CurrentLevelName;
     uint8_t     CurrentLevelID;
-    
+
     uint8_t     Opcode;
     uint8_t     Operand;
-    
+
     bool        NextAction;
 } gameflow_manager_t, *gameflow_manager_p;
 
 enum TR_GAMEFLOW_OP
 {
-    TR_GAMEFLOW_OP_UNKNOWN1,
-    TR_GAMEFLOW_OP_UNKNOWN2,
-    TR_GAMEFLOW_OP_UNKNOWN3,
-    TR_GAMEFLOW_OP_PLAYFMV,
-    TR_GAMEFLOW_OP_PLAYLEVEL,
-    TR_GAMEFLOW_OP_PLAYCUTSCENE,
-    TR_GAMEFLOW_OP_SHOWSTATS,
-    TR_GAMEFLOW_OP_PLAYDEMO,
-    TR_GAMEFLOW_OP_UNKNOWN4,
-    TR_GAMEFLOW_OP_ENDSET,
-    TR_GAMEFLOW_OP_PLAYSOUNDTRACK,
-    TR_GAMEFLOW_OP_LARASTARTANIM,
-    TR_GAMEFLOW_OP_SHOWCHAPTERS,
-    TR_GAMEFLOW_OP_UNKNOWN5,
-    TR_GAMEFLOW_OP_TAKEWEAPONS,
-    TR_GAMEFLOW_OP_ENDGAME,
-    TR_GAMEFLOW_OP_PREPARECUTSCENE,
-    TR_GAMEFLOW_OP_UNKNOWN6,
-    TR_GAMEFLOW_OP_GIVEITEM,
-    TR_GAMEFLOW_OP_SETITEMSTATE,
-    TR_GAMEFLOW_OP_SETNUMSECRETS,
-    TR_GAMEFLOW_OP_UNKNOWN7,
-    TR_GAMEFLOW_OP_TAKESUPPLIES,
-    TR_GAMEFLOW_OP_LASTINDEX
+    TR_GAMEFLOW_OP_PICTURE,         //Unknown possibly TR1?
+    TR_GAMEFLOW_OP_LISTSTART,       //Unknown possibly TR1?
+    TR_GAMEFLOW_OP_LISTEND,         //Unknown possibly TR1?
+    TR_GAMEFLOW_OP_STARTFMV,        //Start a FMV
+    TR_GAMEFLOW_OP_STARTLEVEL,      //Start a level
+    TR_GAMEFLOW_OP_STARTCINE,       //Start a cutscene
+    TR_GAMEFLOW_OP_LEVELCOMPLETE,   //Trigger level completion display
+    TR_GAMEFLOW_OP_STARTDEMO,       //Start a demo level
+    TR_GAMEFLOW_OP_JUMPTOSEQUENCE,  //Jump to an existing sequence
+    TR_GAMEFLOW_OP_ENDSEQUENCE,     //End current sequence
+    TR_GAMEFLOW_OP_SETTRACK,        //Set audio track
+    TR_GAMEFLOW_OP_ENABLESUNSET,    //??? Used on Bartoli's hideout!
+    TR_GAMEFLOW_OP_LOADINGPIC,      //Set loading screen picture
+    TR_GAMEFLOW_OP_DEADLYWATER,     //Set water kills lara (Used on that Rig level, Temple of Xian etc..)
+    TR_GAMEFLOW_OP_REMOVEWEAPONS,   //Remove Lara's weapons
+    TR_GAMEFLOW_OP_GAMECOMPLETE,    //Trigger game completion display
+    TR_GAMEFLOW_OP_CUTANGLE,        //Cutscene start angle? Possibly rotation flags? Unknown!
+    TR_GAMEFLOW_OP_NOFLOOR,         //No floor, unknown used on All Hallows, Thames Wharf, Floating Islands etc!
+    TR_GAMEFLOW_OP_ADDTOINVENTORY,  //Add an item to inventoru
+    TR_GAMEFLOW_OP_LARASTARTANIM,   //Change Lara's start anim or the state? (Used on levels where Lara starts in water
+    TR_GAMEFLOW_OP_NUMSECRETS,      //Change the number of secrets?
+    TR_GAMEFLOW_OP_KILLTOCOMPLETE,  //Kill to complete, used on levels like IcePalace, Nightmare in Vegas so killing the boss ends the level!
+    TR_GAMEFLOW_OP_REMOVEAMMO,      //Remove Ammo
+    TR_GAMEFLOW_OP_LASTINDEX        //???
 };
 
 extern gameflow_manager_s gameflow_manager;
