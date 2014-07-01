abc5f7b0a357591a8129e990692aa2cc1fd9db55
Anatoly Grishin <Anatoly Grishin@localhost>
2014-07-02 01:27:24 +0400

    Script rollback

--- scripts/gameflow/gameflow.lua
+++ scripts/gameflow/gameflow.lua
@@ -5,67 +5,84 @@
 
 print("Gameflow path script loaded");
 
---------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
 -- 1 - Define global constants
---------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
 gameflow_lara_home_index   =  99; -- used to load Lara's Home.
---------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
 -- 2 - Define script path information
---------------------------------------------------------------
-gameflow_script_paths = {};
---------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
+gameflow_paths = {};
+---------------------------------------------------------------------------------------------------------------------------------------
 -- 3 - Assign script path information
---------------------------------------------------------------
-gameflow_script_paths[0]   = "scripts/gameflow/TR1.lua";
-gameflow_script_paths[1]   = "scripts/gameflow/TR1_demo.lua";
-gameflow_script_paths[2]   = "scripts/gameflow/TR1_gold.lua";
-gameflow_script_paths[3]   = "scripts/gameflow/TR2.lua";
-gameflow_script_paths[4]   = "scripts/gameflow/TR2_demo.lua";
-gameflow_script_paths[5]   = "scripts/gameflow/TR3.lua";
-gameflow_script_paths[6]   = "scripts/gameflow/TR4.lua";
-gameflow_script_paths[7]   = "scripts/gameflow/TR4_demo.lua";
-gameflow_script_paths[8]   = "scripts/gameflow/TR5.lua";
-
--- Gold games require seperate script entry.
-
-gameflow_script_paths[9]   = "scripts/gameflow/TR2_gold.lua";
-gameflow_script_paths[10]  = "scripts/gameflow/TR3_gold.lua";
---------------------------------------------------------------
--- This function sets current game, according to passed string
---------------------------------------------------------------
-function GetGameflowScriptPath(game_name)
-	if(game_name == "TR1")  then 
-		cvars.game_script = gameflow_script_paths[0];
-	elseif(game_name == "TR2") then
-		cvars.game_script = gameflow_script_paths[3];
-	elseif(game_name == "TR3") then
-		cvars.game_script = gameflow_script_paths[5];
-	elseif(game_name == "TR4") then
-		cvars.game_script = gameflow_script_paths[6];
-	elseif(game_name == "TR5") then
-		cvars.game_script = gameflow_script_paths[8];
-	elseif(game_name == "TR2GOLD") then
-		cvars.game_script = gameflow_script_paths[9];
-	elseif(game_name == "TR3GOLD") then
-		cvars.game_script = gameflow_script_paths[10];
+---------------------------------------------------------------------------------------------------------------------------------------
+gameflow_paths[1.0] = {script = "scripts/gameflow/TR1.lua", 	 title = "data/tr1/pix/AMERTIT.jpg"		 };
+gameflow_paths[1.1] = {script = "scripts/gameflow/TR1_demo.lua", title = "data/tr1/pix/AMERTIT.jpg"		 };
+gameflow_paths[1.5] = {script = "scripts/gameflow/TR1_gold.lua", title = "data/tr1_gold/pix/TITLE.jpg"	 };
+gameflow_paths[2.0] = {script = "scripts/gameflow/TR2.lua", 	 title = "data/tr2/pix/TITLE.jpg"		 };
+gameflow_paths[2.1] = {script = "scripts/gameflow/TR2_demo.lua", title = "data/tr2/pix/TITLE.jpg"		 };
+gameflow_paths[2.5] = {script = "scripts/gameflow/TR2_gold.lua", title = "data/tr2_gold/pix/TITLE.jpg"	 };
+gameflow_paths[3.0] = {script = "scripts/gameflow/TR3.lua", 	 title = "graphics/tr3_title.png"		 };
+gameflow_paths[3.5] = {script = "scripts/gameflow/TR3_gold.lua", title = "data/tr3_gold/pix/Titleuk.bmp" };
+gameflow_paths[4.0] = {script = "scripts/gameflow/TR4.lua", 	 title = "graphics/tr4_title.png"		 };		-- No TR4 title screen!
+gameflow_paths[4.1] = {script = "scripts/gameflow/TR4_demo.lua", title = "graphics/tr4_title.png"		 };		-- No TR4 title screen!
+gameflow_paths[5.0] = {script = "scripts/gameflow/TR5.lua", 	 title = "graphics/tr5_title.png"		 };		-- No TR5 title screen!
+---------------------------------------------------------------------------------------------------------------------------------------
+-- This function gets current game script path.
+---------------------------------------------------------------------------------------------------------------------------------------
+function GetGameflowScriptPath(game_version)
+	if(game_version <= 5.0) then
+		cvars.game_script = gameflow_paths[game_version].script;
 	else
 		cvars.game_script = "NULL";
 	end;
 end;
-
-function GetNextLevel(currentlevel, operand)
-	if(operand == 0) then -- If zero we just get the next level (TR1/2/3/5)
+---------------------------------------------------------------------------------------------------------------------------------------
+-- Get game's title screen.
+---------------------------------------------------------------------------------------------------------------------------------------
+function GetTitleScreen(game_version)
+	if(game_version <= 5.0) then
+		return gameflow_paths[game_version].title;
+	else
+		return "NULL";
+	end;
+end;
+---------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
+-- This function is a general function to get next level number. Used for both GetNextLevel and GetLoadingScreen functions.
+---------------------------------------------------------------------------------------------------------------------------------------
+function NextLevelNum(currentlevel, operand)
+	local nextlevel;
+	
+	if(operand == 0) then    -- If zero we just get the next level (TR1/2/3/5)
 		nextlevel = currentlevel + 1;
 	else
 		nextlevel = operand; -- Or we load the level id from the level end triggers operand (TR4)
 	end;
 
-	if((nextlevel == gameflow_lara_home_index) and (level[0] ~= nil)) then
+	if((nextlevel == gameflow_lara_home_index) and (level[0] ~= nil)) then	-- Load Lara's Home level, if exist.
 		nextlevel = 0;
-	elseif(nextlevel > numlevels) then -- If End level load first
+	elseif(nextlevel > numlevels) then -- No Lara's Home, load first level instead.
 		nextlevel = 1;
 	end;
 	
-	return level[nextlevel].filepath, level[nextlevel].name, nextlevel;
+	return nextlevel;
 end;
---------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------
+-- Get next level.
+---------------------------------------------------------------------------------------------------------------------------------------
+function GetNextLevel(currentlevel, operand)
+	local level_number = NextLevelNum(currentlevel, operand);
+	if(level[level_number] ~= nil) then
+		return level[level_number].filepath, level[level_number].name, level_number;
+	else
+		return "none", "none", -1;
+	end;
+end;
+---------------------------------------------------------------------------------------------------------------------------------------
+-- Get next level's loading screen.
+---------------------------------------------------------------------------------------------------------------------------------------
+function GetLoadingScreen(currentlevel, operand)
+	local level_number = NextLevelNum(currentlevel, operand);
+	return level[level_number].picpath;
+end;
