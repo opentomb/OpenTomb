f68af9f82e4c7c565dd423631bc2873438f9d2c1
Anatoly Grishin <Anatoly Grishin@localhost>
2014-10-01 01:29:46 +0400

    Added missing files.

---
+++ scripts/inventory/item_list.lua
@@ -0,0 +1,53 @@
+-- OPENTOMB INVENTORY ITEM LIST
+-- by Lwmte, Sep 2014
+
+--------------------------------------------------------------------------------
+-- Defines global inventory items.
+-- Since different versions of TR feature different weapons yet originating from
+-- the previous one (e. g., Automags originating from Magnums, MP5 originating
+-- from M16, etc.), it unifies similar weapons from various games under ancestor
+-- name. For example, ITEM_MAGNUMS count either for magnums, automags, desert
+-- eagle and revolver.
+--------------------------------------------------------------------------------
+
+ITEM_COMPASS = 0
+ITEM_PASSPORT = 1 
+ITEM_VIDEO = 2
+ITEM_AUDIO = 3
+ITEM_CONTROLS = 4   
+ITEM_LOAD = 5
+ITEM_SAVE = 6
+
+ITEM_PISTOLS = 10
+ITEM_SHOTGUN = 11
+ITEM_MAGNUMS = 12
+ITEM_UZIS = 13
+ITEM_M16 = 14
+ITEM_GRENADEGUN = 15
+ITEM_ROCKETGUN = 16
+ITEM_HARPOONGUN = 17
+ITEM_CROSSBOW = 18
+ITEM_GRAPPLEGUN = 19
+
+ITEM_LASERSIGHT = 20
+ITEM_BINOCULARS = 21
+
+ITEM_PISTOL_AMMO = 30
+ITEM_SHOTGUN_NORMAL_AMMO = 31
+ITEM_SHOTGUN_WIDESHOT_AMMO = 32
+ITEM_MAGNUM_AMMO = 33
+ITEM_UZI_AMMO = 34
+ITEM_M16_AMMO = 35
+ITEM_GRENADEGUN_AMMO = 36
+ITEM_ROCKETGUN_AMMO = 37
+ITEM_HARPOONGUN_AMMO = 38
+ITEM_CROSSBOW_NORMAL_AMMO = 39
+ITEM_CROSSBOW_POISON_AMMO = 40
+ITEM_CROSSBOW_EXPLOSIVE_AMMO = 41
+ITEM_GRAPPLEGUN_AMMO = 42
+
+ITEM_FLARES = 43
+ITEM_SINGLE_FLARE = 44
+
+ITEM_SMALL_MEDIPACK = 50
+ITEM_LARGE_MEDIPACK = 51
