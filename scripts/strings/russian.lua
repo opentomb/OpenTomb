a0784f7db62f7d32caf54a34eae9aec25a1f431d
Anatoly Grishin <Anatoly Grishin@localhost>
2015-01-11 05:49:15 +0300

    Added basic strings for menu - English and Russian.

---
+++ scripts/strings/russian.lua
@@ -0,0 +1,96 @@
+-- OPENTOMB GENERIC STRINGS - RUSSIAN LANGUAGE
+-- by Lwmte, Jan 2015
+
+--------------------------------------------------------------------------------
+-- This set of strings is used globally in all engine versions for various
+-- menu entries, notify pop-ups, inventory entries etc.
+--------------------------------------------------------------------------------
+
+strings = {};
+
+-- Game menu entries
+
+strings[000] = "Новая игра";
+strings[001] = "Выбор игры";
+strings[002] = "Выбор уровня";
+strings[003] = "Дом Лары";
+strings[004] = "Сохранить";
+strings[005] = "Загрузить";
+strings[006] = "Настройки";
+strings[007] = "Выход";
+strings[008] = "Переиграть уровень";
+strings[009] = "Выход в главное меню";
+
+-- Dialog components
+
+strings[010] = "Да";
+strings[011] = "Нет";
+strings[012] = "Применить";
+strings[013] = "Отмена";
+strings[014] = "Назад";
+strings[015] = "Вперед";
+strings[016] = "ОК";
+strings[017] = "Сбросить";
+
+-- Interface headers
+
+strings[018] = "ИНВЕНТАРЬ";
+strings[019] = "ДНЕВНИК";
+strings[020] = "ПАУЗА";
+strings[021] = "СТАТИСТИКА";
+strings[022] = "ЗАГРУЗКА";
+
+-- Dialogs
+
+strings[023] = "Выйти из игры?";
+strings[024] = "Выберите файл для загрузки";
+strings[025] = "Выберите файл для сохранения";
+strings[026] = "С чем комбинировать?";
+
+-- Inventory actions
+
+strings[027] = "Выбрать";
+strings[028] = "Сменить боеприпасы";
+strings[029] = "Сменить режим огня";
+strings[030] = "Использовать";
+strings[031] = "Комбинировать";
+strings[032] = "Разделить";
+strings[033] = "Осмотреть";
+strings[034] = "Выкинуть";
+
+-- Interface hints
+
+strings[035] = " - Применить";
+strings[036] = " - Отмена";
+strings[037] = "Зажмите левую кнопку мыши и двигайте мышь для вращения инвентаря.";
+strings[038] = "Прокрутите колесо мыши вверх или вниз для смены режима инвентаря.";
+strings[039] = "Зажмите правую кнопку мыши и двигайте мышь для вращения предмета.";
+
+-- Statistics bar
+
+strings[040] = "Место: %s";
+strings[041] = "Секретов: %d / %d";
+strings[042] = "Пройдено: %d km";
+strings[043] = "Выстрелов: %d";
+strings[044] = "Убийств: %d";
+strings[045] = "Использовано аптечек: %d";
+
+-- Notify pop-up tips - FIND A WAY TO USE THEM!
+
+strings[046] = "Нажмите РАЗВОРОТ в приседе для выполнения кувырка.";
+strings[047] = "В движке OpenTomb Лара скользит строго в направлении наклона";
+strings[048] = "Знали ли Вы, что над OpenTomb в разное время работали минимум 10 человек?";
+strings[049] = "Знали ли Вы, что Core Design никогда не опубликовали исходные коды Tomb Raider?";
+
+-- Game names - to use with Select Game menu...
+
+strings[100] = "Tomb Raider I";
+strings[101] = "Tomb Raider I Gold: Unfinished Business";
+strings[200] = "Tomb Raider II";
+strings[201] = "Tomb Raider II Gold: The Golden Mask";
+strings[300] = "Tomb Raider III";
+strings[301] = "Tomb Raider III Gold: The Lost Artifact";
+strings[400] = "Tomb Raider IV: The Last Revelation";
+strings[401] = "Tomb Raider IV Bonus: The Times Level";
+strings[500] = "Tomb Raider V: Chronicles";
+strings[600] = "Custom Levels";
