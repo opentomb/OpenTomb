4cb36f7cd06f0e0ce38d56ea129e89392ead7ae1
TeslaRus <tesla.rus@gmail.com>
2015-04-24 13:38:09 +0400

    Updated scripts/autoexec.lua script;

--- scripts/autoexec.lua
+++ scripts/autoexec.lua
@@ -25,6 +25,12 @@ dofile("scripts/inventory/items.lua");
 --     3  |__| |__|  6
 ----------------------------
 
+-- 0x00: no overriding / adding;
+-- 0x01: overriding mesh in armed state;
+-- 0x02: add mesh to slot in armed state;
+-- 0x03: overriding mesh in disarmed state;
+-- 0x04: add mesh to slot in disarmed state;
+
 -- creates map for left and right hands
 function setDefaultModelAnimReplaceFlag(m_id)
     setModelAnimReplaceFlag(m_id, 8, 0x01);
@@ -35,78 +41,82 @@ function setDefaultModelAnimReplaceFlag(m_id)
     setModelAnimReplaceFlag(m_id, 13, 0x01);
 end
 
+function test(w, a)
+    setWeaponModel(player, w, a);
+end
+
 if (getLevelVersion() < TR_II) then
     -- pistols
     setDefaultModelAnimReplaceFlag(1);
     setModelMeshReplaceFlag(1, 10, 0x01);
     setModelMeshReplaceFlag(1, 13, 0x01);
-    setModelMeshReplaceFlag(1, 1, 0x02);
-    setModelMeshReplaceFlag(1, 4, 0x02);
+    setModelMeshReplaceFlag(1, 1, 0x03);
+    setModelMeshReplaceFlag(1, 4, 0x03);
     
     -- shotgun
     setDefaultModelAnimReplaceFlag(2);
     setModelMeshReplaceFlag(2, 10, 0x01);
     setModelMeshReplaceFlag(2, 13, 0x01);
-    setModelMeshReplaceFlag(2, 7, 0x02);
+    setModelMeshReplaceFlag(2, 7, 0x03);
 
     -- auto pistols
     setDefaultModelAnimReplaceFlag(3);
     setModelMeshReplaceFlag(3, 10, 0x01);
     setModelMeshReplaceFlag(3, 13, 0x01);
-    setModelMeshReplaceFlag(3, 1, 0x02);
-    setModelMeshReplaceFlag(3, 4, 0x02);
+    setModelMeshReplaceFlag(3, 1, 0x03);
+    setModelMeshReplaceFlag(3, 4, 0x03);
 
     -- UZI
     setDefaultModelAnimReplaceFlag(4);
     setModelMeshReplaceFlag(4, 10, 0x01);
     setModelMeshReplaceFlag(4, 13, 0x01);
     setModelMeshReplaceFlag(4, 14, 0x01);
-    setModelMeshReplaceFlag(4, 1, 0x02);
-    setModelMeshReplaceFlag(4, 4, 0x02);
+    setModelMeshReplaceFlag(4, 1, 0x03);
+    setModelMeshReplaceFlag(4, 4, 0x03);
 
 elseif (getLevelVersion() < TR_III) then
     -- pistols
     setDefaultModelAnimReplaceFlag(1);
     setModelMeshReplaceFlag(1, 10, 0x01);
     setModelMeshReplaceFlag(1, 13, 0x01);
-    setModelMeshReplaceFlag(1, 1, 0x02);
-    setModelMeshReplaceFlag(1, 4, 0x02);
+    setModelMeshReplaceFlag(1, 1, 0x03);
+    setModelMeshReplaceFlag(1, 4, 0x03);
     
     -- shotgun
     setDefaultModelAnimReplaceFlag(3);
     setModelMeshReplaceFlag(3, 10, 0x01);
     setModelMeshReplaceFlag(3, 13, 0x01);
-    setModelMeshReplaceFlag(3, 14, 0x03);
+    setModelMeshReplaceFlag(3, 14, 0x04);
 
     -- auto pistols
     setDefaultModelAnimReplaceFlag(4);
     setModelMeshReplaceFlag(4, 10, 0x01);
     setModelMeshReplaceFlag(4, 13, 0x01);
-    setModelMeshReplaceFlag(4, 1, 0x02);
-    setModelMeshReplaceFlag(4, 4, 0x02);
+    setModelMeshReplaceFlag(4, 1, 0x03);
+    setModelMeshReplaceFlag(4, 4, 0x03);
 
     -- UZI
     setDefaultModelAnimReplaceFlag(5);
     setModelMeshReplaceFlag(5, 10, 0x01);
     setModelMeshReplaceFlag(5, 13, 0x01);
     setModelMeshReplaceFlag(5, 14, 0x01);
-    setModelMeshReplaceFlag(5, 1, 0x02);
-    setModelMeshReplaceFlag(5, 4, 0x02);
+    setModelMeshReplaceFlag(5, 1, 0x03);
+    setModelMeshReplaceFlag(5, 4, 0x03);
 
     -- M16
     setDefaultModelAnimReplaceFlag(6);
     setModelMeshReplaceFlag(6, 10, 0x01);
-    setModelMeshReplaceFlag(6, 14, 0x03);
+    setModelMeshReplaceFlag(6, 14, 0x04);
 
     -- grenade launcher
     setDefaultModelAnimReplaceFlag(7);
     setModelMeshReplaceFlag(7, 10, 0x01);
-    setModelMeshReplaceFlag(7, 14, 0x03);
+    setModelMeshReplaceFlag(7, 14, 0x04);
 
     -- harpoon gun
     setDefaultModelAnimReplaceFlag(8);
     setModelMeshReplaceFlag(8, 10, 0x01);
-    setModelMeshReplaceFlag(8, 14, 0x03);      -- 0x03 slot mesh model: draws with original mesh
+    setModelMeshReplaceFlag(8, 14, 0x04);      -- 0x04 slot mesh model: draws with original mesh
 
     -- flare
     setModelAnimReplaceFlag(9, 11, 0x01);
@@ -118,46 +128,46 @@ elseif (getLevelVersion() < TR_IV) then
     setDefaultModelAnimReplaceFlag(1);
     setModelMeshReplaceFlag(1, 10, 0x01);
     setModelMeshReplaceFlag(1, 13, 0x01);
-    setModelMeshReplaceFlag(1, 1, 0x02);
-    setModelMeshReplaceFlag(1, 4, 0x02);
+    setModelMeshReplaceFlag(1, 1, 0x03);
+    setModelMeshReplaceFlag(1, 4, 0x03);
     
     -- shotgun
     setDefaultModelAnimReplaceFlag(3);
     setModelMeshReplaceFlag(3, 10, 0x01);
-    setModelMeshReplaceFlag(3, 14, 0x03);
+    setModelMeshReplaceFlag(3, 14, 0x04);
 
     -- magnum
     setDefaultModelAnimReplaceFlag(4);
     setModelMeshReplaceFlag(4, 10, 0x01);
-    setModelMeshReplaceFlag(4, 4, 0x02);
+    setModelMeshReplaceFlag(4, 4, 0x03);
 
     -- UZI
     setDefaultModelAnimReplaceFlag(5);
     setModelMeshReplaceFlag(5, 10, 0x01);
     setModelMeshReplaceFlag(5, 13, 0x01);
     setModelMeshReplaceFlag(5, 14, 0x01);
-    setModelMeshReplaceFlag(5, 1, 0x02);
-    setModelMeshReplaceFlag(5, 4, 0x02);
+    setModelMeshReplaceFlag(5, 1, 0x03);
+    setModelMeshReplaceFlag(5, 4, 0x03);
 
     -- M16
     setDefaultModelAnimReplaceFlag(6);
     setModelMeshReplaceFlag(6, 10, 0x01);
-    setModelMeshReplaceFlag(6, 14, 0x03);
+    setModelMeshReplaceFlag(6, 14, 0x04);
 
     -- rocket launcher
     setDefaultModelAnimReplaceFlag(7);
     setModelMeshReplaceFlag(7, 10, 0x01);
-    setModelMeshReplaceFlag(7, 14, 0x03);
+    setModelMeshReplaceFlag(7, 14, 0x04);
 
     -- grenade launcher
     setDefaultModelAnimReplaceFlag(8);
     setModelMeshReplaceFlag(8, 10, 0x01);
-    setModelMeshReplaceFlag(8, 14, 0x03);
+    setModelMeshReplaceFlag(8, 14, 0x04);
 
     -- harpoon gun
     setDefaultModelAnimReplaceFlag(9);
     setModelMeshReplaceFlag(9, 10, 0x01);
-    setModelMeshReplaceFlag(9, 14, 0x03);
+    setModelMeshReplaceFlag(9, 14, 0x04);
 
     -- flare
     setModelAnimReplaceFlag(10, 11, 0x01);
@@ -165,82 +175,45 @@ elseif (getLevelVersion() < TR_IV) then
     setModelAnimReplaceFlag(10, 13, 0x01);
     setModelMeshReplaceFlag(10, 13, 0x01);
 
-elseif (getLevelVersion() < TR_V) then
+elseif (getLevelVersion() <= TR_V) then
     -- pistols
-    --copyMeshFromModelToModel(1, 13, 1);
-    --copyMeshFromModelToModel(1, 13, 4);
+    copyMeshFromModelToModel(1, 14, 1, 4);
+    copyMeshFromModelToModel(1, 14, 4, 8);
     setDefaultModelAnimReplaceFlag(1);
     setModelMeshReplaceFlag(1, 10, 0x01);
     setModelMeshReplaceFlag(1, 13, 0x01);
-    --setModelMeshReplaceFlag(1, 1, 0x02);
-    --setModelMeshReplaceFlag(1, 4, 0x02);
+    setModelMeshReplaceFlag(1, 1, 0x04);
+    setModelMeshReplaceFlag(1, 4, 0x04);
     
     -- UZI
+    copyMeshFromModelToModel(2, 15, 1, 4);
+    copyMeshFromModelToModel(2, 15, 4, 8);
     setDefaultModelAnimReplaceFlag(2);
     setModelMeshReplaceFlag(2, 10, 0x01);
     setModelMeshReplaceFlag(2, 13, 0x01);
-    --setModelMeshReplaceFlag(2, 1, 0x02);
-    --setModelMeshReplaceFlag(2, 4, 0x02);
+    setModelMeshReplaceFlag(2, 1, 0x04);
+    setModelMeshReplaceFlag(2, 4, 0x04);
 
     -- shotgun
     setDefaultModelAnimReplaceFlag(3);
     setModelMeshReplaceFlag(3, 10, 0x01);
-    setModelMeshReplaceFlag(3, 14, 0x03);
+    setModelMeshReplaceFlag(3, 14, 0x04);
 
     -- crossbow
     setDefaultModelAnimReplaceFlag(4);
     setModelMeshReplaceFlag(4, 10, 0x01);
-    setModelMeshReplaceFlag(4, 14, 0x03);
+    setModelMeshReplaceFlag(4, 14, 0x04);
 
     -- grenade launcher
     setDefaultModelAnimReplaceFlag(5);
     setModelMeshReplaceFlag(5, 10, 0x01);
-    setModelMeshReplaceFlag(5, 14, 0x03);
+    setModelMeshReplaceFlag(5, 14, 0x04);
 
     -- magnum
+    copyMeshFromModelToModel(6, 16, 4, 8);
     setDefaultModelAnimReplaceFlag(6);
     setModelMeshReplaceFlag(6, 10, 0x01);
-    --setModelMeshReplaceFlag(6, 4, 0x02);
-
-    -- flare
-    setModelAnimReplaceFlag(7, 11, 0x01);
-    setModelAnimReplaceFlag(7, 12, 0x01);
-    setModelAnimReplaceFlag(7, 13, 0x01);
-    setModelMeshReplaceFlag(7, 13, 0x01);
-elseif (getLevelVersion() == TR_V) then
-    -- pistols
-    setDefaultModelAnimReplaceFlag(1);
-    setModelMeshReplaceFlag(1, 10, 0x01);
-    setModelMeshReplaceFlag(1, 13, 0x01);
-    --setModelMeshReplaceFlag(1, 1, 0x02);
-    --setModelMeshReplaceFlag(1, 4, 0x02);
-    
-    -- UZI
-    setDefaultModelAnimReplaceFlag(2);
-    setModelMeshReplaceFlag(2, 10, 0x01);
-    setModelMeshReplaceFlag(2, 13, 0x01);
-    --setModelMeshReplaceFlag(2, 1, 0x02);
-    --setModelMeshReplaceFlag(2, 4, 0x02);
-
-    -- shotgun
-    setDefaultModelAnimReplaceFlag(3);
-    setModelMeshReplaceFlag(3, 10, 0x01);
-    setModelMeshReplaceFlag(3, 14, 0x03);
-
-    -- crossbow
-    setDefaultModelAnimReplaceFlag(4);
-    setModelMeshReplaceFlag(4, 10, 0x01);
-    setModelMeshReplaceFlag(4, 14, 0x03);
-
-    -- grenade launcher
-    setDefaultModelAnimReplaceFlag(5);
-    setModelMeshReplaceFlag(5, 10, 0x01);
-    setModelMeshReplaceFlag(5, 14, 0x03);
-
-    -- magnum
-    setDefaultModelAnimReplaceFlag(6);
-    setModelMeshReplaceFlag(6, 10, 0x01);
-    --setModelMeshReplaceFlag(6, 4, 0x02);
+    setModelMeshReplaceFlag(6, 4, 0x04);
 
     -- flare
     setModelAnimReplaceFlag(7, 11, 0x01);
@@ -266,23 +239,23 @@ setModelCollisionMap(0, 10, 9);
 
 -- dir[anim = 147, frame = 0, frames = 16]
 setAnimCommandTransform(0, 147, 0, 0x00);   -- roll animation smooth fix 
-setAnimCommandTransform(0, 146, -1, 0x02);
+setAnimCommandTransform(0, 146, -1, 0x03);
 
 setAnimCommandTransform(0, 147, 0, 0x00);   -- roll animation smooth fix 
-setAnimCommandTransform(0, 146, -1, 0x02);
+setAnimCommandTransform(0, 146, -1, 0x03);
 
 setAnimCommandTransform(0, 205, 1, 0x00);   -- underwater roll animation smooth fix 
 setAnimCommandTransform(0, 205, 0, 0x00);
-setAnimCommandTransform(0, 203, -1, 0x02);
+setAnimCommandTransform(0, 203, -1, 0x03);
 
 --setAnimCommandTransform(0, 207, 13, 0x00);   -- jump roll animation smooth fix 
---setAnimCommandTransform(0, 207, 12, 0x02);
+--setAnimCommandTransform(0, 207, 12, 0x03);
 
 --setAnimCommandTransform(0, 210, 8, 0x00);   -- standing jump roll animation smooth fix 
---setAnimCommandTransform(0, 210, 7, 0x02);
+--setAnimCommandTransform(0, 210, 7, 0x03);
 
 setAnimCommandTransform(0, 212, 10, 0x00);   -- back jump roll animation smooth fix 
-setAnimCommandTransform(0, 212, 9, 0x02);
+setAnimCommandTransform(0, 212, 9, 0x03);
 
 -- generate UV rotation texture animations for waterfalls in TR4+ versions
 if (getLevelVersion() == TR_IV) then
