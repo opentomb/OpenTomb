ea720fd820fb42577fe564e55d647d653c53948e
cochrane <torsten@ferroequinologist.de>
2015-07-03 17:47:32 +0200

    Change includes to avoid QuickDraw
    QuickDraw is part of what ApplicationServices pulls in. Who knew? Not me. This caused an issue here because suddenly we had two structs/classes named "Polygon".

--- src/common.cpp
+++ src/common.cpp
@@ -3,7 +3,9 @@
 #ifndef __APPLE_CC__
 #include <SDL2/SDL_image.h>
 #else
-#include <ApplicationServices/ApplicationServices.h>
+#include <CoreGraphics/CoreGraphics.h>
+#include <ImageIO/ImageIO.h>
+#include <CoreServices/CoreServices.h>
 #endif
 #include <SDL2/SDL_opengl.h>
 #include <stdio.h>
