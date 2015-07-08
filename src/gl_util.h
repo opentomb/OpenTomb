a335f111cee17ff42ea16c11804446cb952eb5b7
cochrane <torsten@ferroequinologist.de>
2015-07-08 20:53:19 +0200

    Improve OpenGL logging
    Add filename and location of where errors are first seen.

--- src/gl_util.h
+++ src/gl_util.h
@@ -24,7 +24,8 @@
 
 #include <GL/glew.h>
 
-int checkOpenGLError();
+#define checkOpenGLError() checkOpenGLErrorDetailed(__FILE__, __LINE__)
+int checkOpenGLErrorDetailed(const char *file, int line);
 void printShaderInfoLog (GLuint object);
 int loadShaderFromBuff(GLuint ShaderObj, char * source);
 int loadShaderFromFile(GLuint ShaderObj, const char * fileName, const char *additionalDefines);
