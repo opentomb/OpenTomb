ea2d5ab69de60bc79096c15f4297c4df29f53e89
Gh0stBlade <misternightmare@live.com>
2016-08-02 19:18:17 +0100

    Remove redundant code and improve error detection

--- src/gameflow.cpp
+++ src/gameflow.cpp
@@ -19,9 +19,8 @@ CGameflow::CGameflow()
     memset(this->m_currentLevelPath, 0, MAX_ENGINE_PATH);
     this->m_currentGameID = 0;
     this->m_currentLevelID = 0;
-    this->m_nextAction = false;
-    memset(this->m_actions, TR_GAMEFLOW_NOENTRY, TR_GAMEFLOW_MAX_SECRETS);
-    memset(this->m_secretsTriggerMap, 0, TR_GAMEFLOW_MAX_SECRETS);
+    memset(this->m_actions, GF_NOENTRY, GF_MAX_SECRETS);
+    memset(this->m_secretsTriggerMap, 0, GF_MAX_SECRETS);
 }
 
 
@@ -31,72 +30,66 @@ CGameflow::~CGameflow()
     memset(this->m_currentLevelPath, 0, MAX_ENGINE_PATH);
     this->m_currentGameID = 0;
     this->m_currentLevelID = 0;
-    this->m_nextAction = false;
-    memset(this->m_actions, TR_GAMEFLOW_NOENTRY, TR_GAMEFLOW_MAX_SECRETS);
-    memset(this->m_secretsTriggerMap, 0, TR_GAMEFLOW_MAX_SECRETS);
+    memset(this->m_actions, GF_NOENTRY, GF_MAX_SECRETS);
+    memset(this->m_secretsTriggerMap, 0, GF_MAX_SECRETS);
 }
 
 
 void CGameflow::Do()
 {
-    if(!this->m_nextAction)
-        return;
-
-    bool completed = true;
-
-    for(int i = 0; i < TR_GAMEFLOW_MAX_ACTIONS; i++)
+    for(int i = 0; i < GF_MAX_ACTIONS; i++)
     {
         switch(this->m_actions[i].m_opcode)
         {
-            case TR_GAMEFLOW_OP_LEVELCOMPLETE:
+            case GF_OP_LEVELCOMPLETE:
+            {
+                int top = lua_gettop(engine_lua);
+
+                //Must be loaded from gameflow script!
+                lua_getglobal(engine_lua, "getNextLevel");
+
+                //Push the 3 arguments require dby getNextLevel();
+                lua_pushnumber(engine_lua, this->m_currentGameID);
+                lua_pushnumber(engine_lua, this->m_currentLevelID);
+                lua_pushnumber(engine_lua, this->m_actions[i].m_operand);
+
+                if (lua_CallAndLog(engine_lua, 3, 3, 0))
                 {
-                    int top = lua_gettop(engine_lua);
-                    lua_getglobal(engine_lua, "getNextLevel");                       // Must be loaded from gameflow script!
-                    lua_pushnumber(engine_lua, this->m_currentGameID);      // Push the 1st argument
-                    lua_pushnumber(engine_lua, this->m_currentLevelID);     // Push the 2nd argument
-                    lua_pushnumber(engine_lua, this->m_actions[i].m_operand); // Push the 3rd argument
-
-                    if (lua_CallAndLog(engine_lua, 3, 3, 0))
-                    {
-                        this->m_currentLevelID = lua_tonumber(engine_lua, -1);   // First value in stack is level id
-                        strncpy(this->m_currentLevelName, lua_tostring(engine_lua, -2), LEVEL_NAME_MAX_LEN); // Second value in stack is level name
-                        strncpy(this->m_currentLevelPath, lua_tostring(engine_lua, -3), MAX_ENGINE_PATH); // Third value in stack is level path
-                        Engine_LoadMap(this->m_currentLevelPath);
-                    }
-                    else
-                    {
-                        Con_AddLine("Fatal Error: Failed to call GetNextLevel()", FONTSTYLE_CONSOLE_WARNING);
-                    }
-                    lua_settop(engine_lua, top);
-                    this->m_actions[i].m_opcode = TR_GAMEFLOW_NOENTRY;
+                    //First value in stack is level id
+                    this->m_currentLevelID = lua_tonumber(engine_lua, -1);
+                    //Second value in stack is level name
+                    strncpy(this->m_currentLevelName, lua_tostring(engine_lua, -2), LEVEL_NAME_MAX_LEN);
+                    //Third value in stack is level path
+                    strncpy(this->m_currentLevelPath, lua_tostring(engine_lua, -3), MAX_ENGINE_PATH);
+                    Engine_LoadMap(this->m_currentLevelPath);
                 }
-                break;
-
-            case TR_GAMEFLOW_NOENTRY:
+                else
+                {
+                    Con_AddLine("Fatal Error: Failed to call GetNextLevel()", FONTSTYLE_CONSOLE_WARNING);
+                }
+                lua_settop(engine_lua, top);
+                this->m_actions[i].m_opcode = GF_NOENTRY;
+            }
+            break;
+            case GF_NOENTRY:
                 continue;
-
             default:
-                this->m_actions[i].m_opcode = TR_GAMEFLOW_NOENTRY;
-                break;  ///@FIXME: Implement all other gameflow opcodes here!
+                Con_Printf("Unimplemented gameflow opcode: %i", this->m_actions[i].m_opcode);
+                break;
         };   // end switch(gameflow_manager.Operand)
-        completed = false;
     }
-
-    if(completed) this->m_nextAction = false;    // Reset action marker!
 }
 
 
 bool CGameflow::Send(int opcode, int operand)
 {
-    for(int i = 0; i < TR_GAMEFLOW_MAX_ACTIONS; i++)
+    //Iterate through our action list until we have a free space (TR_GAMEFLOW_NOENTRY) to add the next action.
+    for(int i = 0; i < GF_MAX_ACTIONS; i++)
     {
-        if(this->m_actions[i].m_opcode == opcode) return false;
-
-        if(this->m_actions[i].m_opcode == TR_GAMEFLOW_NOENTRY)
+        if(this->m_actions[i].m_opcode == GF_NOENTRY)
         {
-            this->m_actions[i].m_opcode   = opcode;
-            this->m_actions[i].m_operand  = operand;
-            this->m_nextAction            = true;
+            this->m_actions[i].m_opcode = opcode;
+            this->m_actions[i].m_operand = operand;
             return true;
         }
     }
@@ -144,19 +137,19 @@ void CGameflow::setCurrentLevelPath(const char* filePath)
 
 void CGameflow::resetSecrets()
 {
-    memset(this->m_secretsTriggerMap, 0, TR_GAMEFLOW_MAX_SECRETS);
+    memset(this->m_secretsTriggerMap, 0, GF_MAX_SECRETS);
 }
 
 
 char CGameflow::getSecretStateAtIndex(int index)
 {
-    assert((index >= 0) && index <= (TR_GAMEFLOW_MAX_SECRETS));
+    assert((index >= 0) && index <= (GF_MAX_SECRETS));
     return this->m_secretsTriggerMap[index];
 }
 
 void CGameflow::setSecretStateAtIndex(int index, int value)
 {
-    assert((index >= 0) && index <= (TR_GAMEFLOW_MAX_SECRETS));
+    assert((index >= 0) && index <= (GF_MAX_SECRETS));
     this->m_secretsTriggerMap[index] = (char)value; ///@FIXME should not cast.
 }
 
--- src/gameflow.h
+++ src/gameflow.h
@@ -16,10 +16,10 @@
 #define     GAME_4_1    (9)
 #define     GAME_5      (10)
 
-#define TR_GAMEFLOW_MAX_ACTIONS 32
-#define TR_GAMEFLOW_MAX_SECRETS 256
+#define GF_MAX_ACTIONS 32
+#define GF_MAX_SECRETS 256
 
-#define TR_GAMEFLOW_NOENTRY     -1
+#define GF_NOENTRY     -1
 
 struct gameflow_action
 {
@@ -37,6 +37,9 @@ public:
 
     void                Do();
     bool                Send(int opcode, int operand = -1);
+
+    uint16_t            getGameState();
+    void                setGameState(uint16_t state);
 
     uint8_t             getCurrentGameID();
     uint8_t             getCurrentLevelID();
@@ -60,37 +63,36 @@ private:
     char                m_currentLevelName[LEVEL_NAME_MAX_LEN];
     char                m_currentLevelPath[MAX_ENGINE_PATH];
 
-    bool                m_nextAction;
-    gameflow_action     m_actions[TR_GAMEFLOW_MAX_ACTIONS];
-    char                m_secretsTriggerMap[TR_GAMEFLOW_MAX_SECRETS];
+    gameflow_action     m_actions[GF_MAX_ACTIONS];
+    char                m_secretsTriggerMap[GF_MAX_SECRETS];
 };
 
-enum TR_GAMEFLOW_OP
+enum GF_OP
 {
-    TR_GAMEFLOW_OP_PICTURE,         // Unknown possibly TR1?
-    TR_GAMEFLOW_OP_LISTSTART,       // Unknown possibly TR1?
-    TR_GAMEFLOW_OP_LISTEND,         // Unknown possibly TR1?
-    TR_GAMEFLOW_OP_STARTFMV,        // Start a FMV
-    TR_GAMEFLOW_OP_STARTLEVEL,      // Start a level
-    TR_GAMEFLOW_OP_STARTCINE,       // Start a cutscene
-    TR_GAMEFLOW_OP_LEVELCOMPLETE,   // Trigger level completion display
-    TR_GAMEFLOW_OP_STARTDEMO,       // Start a demo level
-    TR_GAMEFLOW_OP_JUMPTOSEQUENCE,  // Jump to an existing sequence
-    TR_GAMEFLOW_OP_ENDSEQUENCE,     // End current sequence
-    TR_GAMEFLOW_OP_SETTRACK,        // Set audio track
-    TR_GAMEFLOW_OP_ENABLESUNSET,    // ??? Used on Bartoli's hideout!
-    TR_GAMEFLOW_OP_LOADINGPIC,      // Set loading screen picture
-    TR_GAMEFLOW_OP_DEADLYWATER,     // Set water kills lara (Used on that Rig level, Temple of Xian etc..)
-    TR_GAMEFLOW_OP_REMOVEWEAPONS,   // Remove Lara's weapons
-    TR_GAMEFLOW_OP_GAMECOMPLETE,    // Trigger game completion display
-    TR_GAMEFLOW_OP_CUTANGLE,        // Cutscene start angle? Possibly rotation flags? Unknown!
-    TR_GAMEFLOW_OP_NOFLOOR,         // Makes Lara infinitely fall at the bottom of the level
-    TR_GAMEFLOW_OP_ADDTOINVENTORY,  // Add an item to inventory
-    TR_GAMEFLOW_OP_LARASTARTANIM,   // Change Lara's start anim or the state? (Used on levels where Lara starts in water)
-    TR_GAMEFLOW_OP_NUMSECRETS,      // Change the number of secrets?
-    TR_GAMEFLOW_OP_KILLTOCOMPLETE,  // Kill to complete, used on levels like IcePalace, Nightmare in Vegas so killing the boss ends the level!
-    TR_GAMEFLOW_OP_REMOVEAMMO,      // Remove Ammo
-    TR_GAMEFLOW_OP_LASTINDEX
+    GF_OP_PICTURE,         // Unknown possibly TR1?
+    GF_OP_LISTSTART,       // Unknown possibly TR1?
+    GF_OP_LISTEND,         // Unknown possibly TR1?
+    GF_OP_STARTFMV,        // Start a FMV
+    GF_OP_STARTLEVEL,      // Start a level
+    GF_OP_STARTCINE,       // Start a cutscene
+    GF_OP_LEVELCOMPLETE,   // Trigger level completion display
+    GF_OP_STARTDEMO,       // Start a demo level
+    GF_OP_JUMPTOSEQUENCE,  // Jump to an existing sequence
+    GF_OP_ENDSEQUENCE,     // End current sequence
+    GF_OP_SETTRACK,        // Set audio track
+    GF_OP_ENABLESUNSET,    // ??? Used on Bartoli's hideout!
+    GF_OP_LOADINGPIC,      // Set loading screen picture
+    GF_OP_DEADLYWATER,     // Set water kills lara (Used on that Rig level, Temple of Xian etc..)
+    GF_OP_REMOVEWEAPONS,   // Remove Lara's weapons
+    GF_OP_GAMECOMPLETE,    // Trigger game completion display
+    GF_OP_CUTANGLE,        // Cutscene start angle? Possibly rotation flags? Unknown!
+    GF_OP_NOFLOOR,         // Makes Lara infinitely fall at the bottom of the level
+    GF_OP_ADDTOINVENTORY,  // Add an item to inventory
+    GF_OP_LARASTARTANIM,   // Change Lara's start anim or the state? (Used on levels where Lara starts in water)
+    GF_OP_NUMSECRETS,      // Change the number of secrets?
+    GF_OP_KILLTOCOMPLETE,  // Kill to complete, used on levels like IcePalace, Nightmare in Vegas so killing the boss ends the level!
+    GF_OP_REMOVEAMMO,      // Remove Ammo
+    GF_OP_LASTINDEX
 };
 
 extern class CGameflow gameflow;
