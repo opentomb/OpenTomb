ee6797206f9376f77050aa763fdfe7b5c9bc76f4
TeslaRus <tesla.rus@gmail.com>
2016-12-10 20:31:48 +0400

    fixed TR1 LEVEL7B collision issue (maybe hacky, but works);

--- autoexec.lua
+++ autoexec.lua
@@ -20,5 +20,5 @@ noclip(0);
 --loadMap(base_path .. "data/LEVEL1.PHD");
 --loadMap(base_path .. "tests/altroom2/LEVEL1.PHD");
 --loadMap(base_path .. "tests/heavy1/LEVEL1.PHD");
-setgamef(1, 14);
---dofile(base_path .. "save/qsave.lua");
+--setgamef(1, 14);
+dofile(base_path .. "save/qsave.lua");
--- scripts/level/tr1/LEVEL7B_autoexec.lua
+++ scripts/level/tr1/LEVEL7B_autoexec.lua
@@ -1,2 +1,4 @@
 
 print("LEVEL7B_autoexec loaded");
+
+addRoomToOverlappedList(77, 78);
