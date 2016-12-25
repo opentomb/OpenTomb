7f6255e359ac85e2931cb3428aa14f199f66e3c8
TeslaRus <tesla.rus@gmail.com>
2016-12-25 23:51:42 +0400

    level 14 scion is not pickable;

--- scripts/level/tr1/LEVEL10B_autoexec.lua
+++ scripts/level/tr1/LEVEL10B_autoexec.lua
@@ -1,3 +1,7 @@
+
+print("LEVEL10B_autoexec.lua loaded");
 playStream(60)
 
 addRoomToOverlappedList(47, 84);
+setEntityTypeFlag(0x6E, ENTITY_TYPE_PICKABLE, 0);
+setEntityTypeFlag(0x6E, ENTITY_TYPE_GENERIC, 1);
