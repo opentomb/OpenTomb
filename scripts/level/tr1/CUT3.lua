bb544e8181da01d6907970d7cc1282172cf7dbb4
TeslaRus <tesla.rus@gmail.com>
2017-08-27 14:25:16 +0400

    added flipmap hack to CUT3;

--- scripts/level/tr1/CUT3.lua
+++ scripts/level/tr1/CUT3.lua
@@ -4,10 +4,14 @@
 print("Level script loaded (CUT3.lua)");
 
 level_PostLoad = function()
+    setGlobalFlipState(1);
     playStream(24);
     entity_funcs[1] = {};
     entity_funcs[1].t = 0;
     entity_funcs[1].onLoop = function(object_id, tick_state)
+        if((entity_funcs[1].t >= 5) and (entity_funcs[1].t < 5 + frame_time)) then
+            setGlobalFlipState(0);
+        end;
         entity_funcs[1].t = entity_funcs[1].t + frame_time;
         if(not setCameraFrame(entity_funcs[1].t * 30)) then
             setGame(GAME_1, 14);
