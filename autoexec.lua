d65a4d44418d11b8472767891bd1e38107065098
TeslaRus <tesla.rus@gmail.com>
2017-09-09 20:48:44 +0400

    added AI disabling option and Doppelganger fixing (again);

--- autoexec.lua
+++ autoexec.lua
@@ -18,9 +18,9 @@ freelook(0);
 cam_distance(1024.0);
 noclip(0);
 --playVideo(base_path .. "data/tr1/fmv/CORE.RPL");
---setgamef(1, 0)
+setgamef(1, 17)
 --loadMap(base_path .. "tests/altroom3/LEVEL1.PHD");
 --loadMap(base_path .. "tests/heavy1/LEVEL1.PHD");
 --loadMap(base_path .. "tests/TRIGGERS.PHD");
 --setgamef(4, 22);
-dofile(base_path .. "save/qsave.lua");
+--dofile(base_path .. "save/qsave.lua");
--- scripts/entity/entity_functions_enemies.lua
+++ scripts/entity/entity_functions_enemies.lua
@@ -158,7 +158,7 @@ function Doppelgagner_init(id)
     setCharacterMoveSizes(id, 768.0, 128.0, 288.0, 1920.0, 320.0); -- height, min_step_up_height, max_step_up_height, max_climb_height, fall_down_height
 
     setEntityMoveType(id, MOVE_ON_FLOOR);
-    setCharacterAIParams(id, -1, ZONE_TYPE_FLY);
+    setCharacterAIParams(id, -1, -1);
 
     local x0 = 36864;
     local y0 = 61440;
