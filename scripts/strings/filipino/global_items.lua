336ade17a495d4da4738216f4d12c45609f6ef48
Cyyy1998 <36368860+Cyyy1998@users.noreply.github.com>
2018-02-16 19:21:55 +0800

    global_items.lua
    Filipino translated

---
+++ scripts/strings/filipino/global_items.lua
@@ -0,0 +1,81 @@
+-- OPENTOMB INVENTORY GLOBAL ITEM NAMES
+-- by Lwmte, Sep 2014
+
+--------------------------------------------------------------------------------
+-- In this file, we list all the global item names used across all games.
+-- Non-global item names should be eventually listed in level-specific scripts.
+-- Note that names list not always resemble item list, as we also introduce
+-- game-specific global item names (e.g. compass was replaced by watch in TR2-3)
+--------------------------------------------------------------------------------
+
+ITEM_NAME_COMPASS = "Compass"
+ITEM_NAME_WATCH = "Istatistika"
+ITEM_NAME_PASSPORT = "Laro
+ITEM_NAME_LARAHOME = "Bahay ni Lara"
+ITEM_NAME_VIDEO = "Mga Setting ng Video"
+ITEM_NAME_AUDIO = "Mga Setting ng Audio"
+ITEM_NAME_CONTROLS = "pagsasaayos ng kontrol"
+ITEM_NAME_LOAD = "I-load ang Laro"
+ITEM_NAME_SAVE = "I-save ang Game"
+ITEM_NAME_MAP = "Mapa"
+
+
+ITEM_NAME_PISTOLS = "Mga Pistol"
+ITEM_NAME_SHOTGUN = "Shotgun"
+ITEM_NAME_MAGNUMS = "Mga Magnum"
+ITEM_NAME_AUTOMAGS = "Mga Awtomatikong Pistola"
+ITEM_NAME_DESERTEAGLE = "Desert Eagle"
+ITEM_NAME_REVOLVER = "Rebolber"
+ITEM_NAME_UZIS = "Uzis"
+ITEM_NAME_M16 = "M16"
+ITEM_NAME_MP5 = "MP5"
+ITEM_NAME_HK = "H&K Gun"
+ITEM_NAME_GRENADEGUN = "Grenade Launcher"
+ITEM_NAME_ROCKETGUN = "Rocket Launcher"
+ITEM_NAME_HARPOONGUN = "Harpoon Gun"
+ITEM_NAME_CROSSBOW = "Pana"
+ITEM_NAME_GRAPPLEGUN = "Pag-ukit ng baril"
+ITEM_NAME_LASERSIGHT = "Lasersight"
+ITEM_NAME_BINOCULARS = "Binokulo"
+ITEM_NAME_SILENCER   = "Silencer"
+
+ITEM_NAME_REVOLVER_LASERSIGHT = "Rebolber + Lasersight"
+ITEM_NAME_CROSSBOW_LASERSIGHT = "Pana + Lasersight"
+ITEM_NAME_HK_LASERSIGHT       = "H&K Gun + Lasersight"
+
+-- We don't need to specify each ammo name, as usually one weapon
+-- has one type of ammo. This was changed since TR4, for which we
+-- specify some ammo names explicitly.
+
+
+ITEM_NAME_PISTOL_AMMO = "Munisyon na Pistol"
+ITEM_NAME_SHOTGUN_AMMO = "Munisyon ng Shotgun"
+ITEM_NAME_SHOTGUN_NORMAL_AMMO = "Karaniwang Muni ng baril"
+TEM_NAME_SHOTGUN_WIDESHOT_AMMO = "Shotgun Wideshot Ammo"
+ITEM_NAME_MAGNUM_AMMO = "Munisyon na magnum"
+ITEM_NAME_AUTOMAG_AMMO = "Awtomatikong Pistol na Ammo"
+ITEM_NAME_DESERTEAGLE_AMMO = "Desert Eagle Ammo"
+ITEM_NAME_REVOLVER_AMMO = "Munisyon na Rebolber"
+ITEM_NAME_UZI_AMMO = "Munisyong na UZI"
+ITEM_NAME_M16_AMMO = "Munisyon na M16"
+ITEM_NAME_MP5_AMMO = "Munisyon na MP5"
+ITEM_NAME_HK_AMMO = "Munisyon ng H&K Gun"
+ITEM_NAME_GRENADEGUN_AMMO = "Grenadegun Ammo"
+
+
+ITEM_NAME_GRENADEGUN_NORMAL_AMMO = "Grenade gun Normal na munisyon"
+ITEM_NAME_GRENADEGUN_SUPER_AMMO = "Grenadegun Super Ammo"
+ITEM_NAME_GRENADEGUN_FLASH_AMMO = "Grenadegun Flash Ammo"
+ITEM_NAME_ROCKETGUN_AMMO = "Rockets"
+ITEM_NAME_HARPOONGUN_AMMO = "Harpoons"
+ITEM_NAME_CROSSBOW_NORMAL_AMMO = "Crossbow Normal Ammo"
+ITEM_NAME_CROSSBOW_POISON_AMMO = "Crossbow Poison Ammo"
+ITEM_NAME_CROSSBOW_EXPLOSIVE_AMMO = "Crossbow Explosive Ammo"
+ITEM_NAME_GRAPPLEGUN_AMMO = "Grappling Gun Ammo"
+
+
+
+ITEM_NAME_FLARES = "Mga flare"
+ITEM_NAME_TORCH  = "Torch"   -- Don't know why we may need this?..
+ITEM_NAME_SMALL_MEDIPACK = "Maliit na Medipack"
+ITEM_NAME_LARGE_MEDIPACK = "Malaking Medipack"
