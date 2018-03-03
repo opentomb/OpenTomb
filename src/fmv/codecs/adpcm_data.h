c8442f97ad62f3291a3f27575ce6af7c321e51b5
carlo-bramini <carlo.bramini@users.sourceforge.net>
2018-03-03 13:37:22 +0100

    Fix type for ff_adpcm_afc_coeffs[][]

--- src/fmv/codecs/adpcm_data.h
+++ src/fmv/codecs/adpcm_data.h
@@ -28,6 +28,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 static const uint8_t ff_adpcm_ima_block_sizes[4]   = {  4, 12, 4, 20 };
 static const uint8_t ff_adpcm_ima_block_samples[4] = { 16, 32, 8, 32 };
 
@@ -42,5 +46,9 @@ extern const int16_t ff_adpcm_yamaha_indexscale[];
 extern const int8_t  ff_adpcm_yamaha_difflookup[];
 extern const int16_t ff_adpcm_afc_coeffs[2][16];
 extern const int16_t ff_adpcm_mtaf_stepsize[32][16];
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* AVCODEC_ADPCM_DATA_H */
