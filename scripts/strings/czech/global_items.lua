8cdf1bed07c655b05d7a3451c2dc73def91d5e53
Clonewayx <Clonewayx@users.noreply.github.com>
2018-04-01 08:15:11 +0200

    Create global_items.lua
    czech translation

---
+++ scripts/strings/czech/global_items.lua
@@ -0,0 +1,77 @@
+-- OPENTOMB INVENTORY GLOBAL ITEM NAMES
+-- by Lwmte, Sep 2014
+
+--------------------------------------------------------------------------------
+-- In this file, we list all the global item names used across all games.
+-- Non-global item names should be eventually listed in level-specific scripts.
+-- Note that names list not always resemble item list, as we also introduce
+-- game-specific global item names (e.g. compass was replaced by watch in TR2-3)
+--------------------------------------------------------------------------------
+
+ITEM_NAME_COMPASS = "Buzola"
+ITEM_NAME_WATCH = "Statistiky"
+ITEM_NAME_PASSPORT = "Hra"
+ITEM_NAME_LARAHOME = "Dům Lary"
+ITEM_NAME_VIDEO = "Nastavení zobrazení"
+ITEM_NAME_AUDIO = "Nastavení zvuku"
+ITEM_NAME_CONTROLS = "Volba ovládání"
+ITEM_NAME_LOAD = "Nahrát hru"
+ITEM_NAME_SAVE = "Uložit hru"
+ITEM_NAME_MAP = "Mapa"
+
+ITEM_NAME_PISTOLS = "Pistole"
+ITEM_NAME_SHOTGUN = "Brokovnice"
+ITEM_NAME_MAGNUMS = "Magnumy"
+ITEM_NAME_AUTOMAGS = "Automatické pistole"
+ITEM_NAME_DESERTEAGLE = "Desert Eagle"
+ITEM_NAME_REVOLVER = "Revolver"
+ITEM_NAME_UZIS = "Uzi"
+ITEM_NAME_M16 = "M16"
+ITEM_NAME_MP5 = "MP5"
+ITEM_NAME_HK = "pistole H&K"
+ITEM_NAME_GRENADEGUN = "Granátomet"
+ITEM_NAME_ROCKETGUN = "Odpalovač raket"
+ITEM_NAME_HARPOONGUN = "Hrapuna"
+ITEM_NAME_CROSSBOW = "Kuš"
+ITEM_NAME_GRAPPLEGUN = "Vystřelovač háku"
+
+ITEM_NAME_LASERSIGHT = "Laserové zaměřování"
+ITEM_NAME_BINOCULARS = "Triedr"
+ITEM_NAME_SILENCER   = "Tlumič"
+
+ITEM_NAME_REVOLVER_LASERSIGHT = "Revolver + Laser"
+ITEM_NAME_CROSSBOW_LASERSIGHT = "Kuše + Laser"
+ITEM_NAME_HK_LASERSIGHT       = "Pistole H&K+ Laser"
+
+-- We don't need to specify each ammo name, as usually one weapon
+-- has one type of ammo. This was changed since TR4, for which we
+-- specify some ammo names explicitly.
+
+ITEM_NAME_PISTOL_AMMO = "Munice do Pistole"
+ITEM_NAME_SHOTGUN_AMMO = "Munice do brokovnice"
+ITEM_NAME_SHOTGUN_NORMAL_AMMO = "Standardní munice do brokovnice"
+ITEM_NAME_SHOTGUN_WIDESHOT_AMMO = "Munice do brokovnice s širokým rozptylem"
+ITEM_NAME_MAGNUM_AMMO = "Munice do Magnumu"
+ITEM_NAME_AUTOMAG_AMMO = "Munice do automatické pistole"
+ITEM_NAME_DESERTEAGLE_AMMO = "Munice do Desert Eagle"
+ITEM_NAME_REVOLVER_AMMO = "Munice do revolveru"
+ITEM_NAME_UZI_AMMO = "Munice do Uzi"
+ITEM_NAME_M16_AMMO = "Munice do M16"
+ITEM_NAME_MP5_AMMO = "Munice do MP5"
+ITEM_NAME_HK_AMMO = "Munice do H&K"
+ITEM_NAME_GRENADEGUN_AMMO = "Munice do granátometu"
+ITEM_NAME_GRENADEGUN_NORMAL_AMMO = "Standardní munice do granátometu"
+ITEM_NAME_GRENADEGUN_SUPER_AMMO = "Prémiová munice do granátometu"
+ITEM_NAME_GRENADEGUN_FLASH_AMMO = "Záblesková munice do granátometu"
+ITEM_NAME_ROCKETGUN_AMMO = "Rakety"
+ITEM_NAME_HARPOONGUN_AMMO = "Harpuny"
+ITEM_NAME_CROSSBOW_NORMAL_AMMO = "Běžné špiky do kuše"
+ITEM_NAME_CROSSBOW_POISON_AMMO = "Otrávené špiky do kuše"
+ITEM_NAME_CROSSBOW_EXPLOSIVE_AMMO = "Výbušné šipky do kuše"
+ITEM_NAME_GRAPPLEGUN_AMMO = "Vystřelovací háky"
+
+ITEM_NAME_FLARES = "Světlice"
+ITEM_NAME_TORCH  = "Louč"   -- Don't know why we may need this?..
+
+ITEM_NAME_SMALL_MEDIPACK = "Malá lékárnička"
+ITEM_NAME_LARGE_MEDIPACK = "Velká lékárnička"
