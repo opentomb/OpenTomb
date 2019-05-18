77b97fea02446afa84c33a1707105e94bf74b948
TokyoSU <vlevasseur5@gmail.com>
2019-05-18 18:15:56 +0200

    [MISC] change tab to space.

--- src/timer.cpp
+++ src/timer.cpp
@@ -6,53 +6,53 @@ static LARGE_INTEGER g_counts_per_sec;
 
 struct timespec
 {
-	time_t tv_sec;
-	long tv_nsec;
+    time_t tv_sec;
+    long tv_nsec;
 };
 
 int _clock_gettime(int dummy, struct timespec *ct)
 {
-	LARGE_INTEGER count;
-	if (g_first_time)
-	{
-		g_first_time = 0;
-		if (0 == QueryPerformanceFrequency(&g_counts_per_sec))
-		{
-			g_counts_per_sec.QuadPart = 0;
-		}
-	}
+    LARGE_INTEGER count;
+    if (g_first_time)
+    {
+        g_first_time = 0;
+        if (0 == QueryPerformanceFrequency(&g_counts_per_sec))
+        {
+            g_counts_per_sec.QuadPart = 0;
+        }
+    }
 
-	if ((NULL == ct) || (g_counts_per_sec.QuadPart <= 0) ||
-		(0 == QueryPerformanceCounter(&count)))
-	{
-		return -1;
-	}
+    if ((NULL == ct) || (g_counts_per_sec.QuadPart <= 0) ||
+        (0 == QueryPerformanceCounter(&count)))
+    {
+        return -1;
+    }
 
-	ct->tv_sec = count.QuadPart / g_counts_per_sec.QuadPart;
-	ct->tv_nsec = ((count.QuadPart % g_counts_per_sec.QuadPart) * BILLION) / g_counts_per_sec.QuadPart;
-	return 0;
+    ct->tv_sec = count.QuadPart / g_counts_per_sec.QuadPart;
+    ct->tv_nsec = ((count.QuadPart % g_counts_per_sec.QuadPart) * BILLION) / g_counts_per_sec.QuadPart;
+    return 0;
 }
 
 int _gettimeofday(struct timeval *p, void *tz)
 {
-	ULARGE_INTEGER ul; // As specified on MSDN.
-	FILETIME ft;
+    ULARGE_INTEGER ul; // As specified on MSDN.
+    FILETIME ft;
 
-	// Returns a 64-bit value representing the number of
-	// 100-nanosecond intervals since January 1, 1601 (UTC).
-	GetSystemTimeAsFileTime(&ft);
+    // Returns a 64-bit value representing the number of
+    // 100-nanosecond intervals since January 1, 1601 (UTC).
+    GetSystemTimeAsFileTime(&ft);
 
-	// Fill ULARGE_INTEGER low and high parts.
-	ul.LowPart = ft.dwLowDateTime;
-	ul.HighPart = ft.dwHighDateTime;
-	// Convert to microseconds.
-	ul.QuadPart /= 10ULL;
-	// Remove Windows to UNIX Epoch delta.
-	ul.QuadPart -= 11644473600000000ULL;
-	// Modulo to retrieve the microseconds.
-	p->tv_usec = (long) (ul.QuadPart % 1000000LL);
-	// Divide to retrieve the seconds.
-	p->tv_sec = (long) (ul.QuadPart / 1000000LL);
+    // Fill ULARGE_INTEGER low and high parts.
+    ul.LowPart = ft.dwLowDateTime;
+    ul.HighPart = ft.dwHighDateTime;
+    // Convert to microseconds.
+    ul.QuadPart /= 10ULL;
+    // Remove Windows to UNIX Epoch delta.
+    ul.QuadPart -= 11644473600000000ULL;
+    // Modulo to retrieve the microseconds.
+    p->tv_usec = (long) (ul.QuadPart % 1000000LL);
+    // Divide to retrieve the seconds.
+    p->tv_sec = (long) (ul.QuadPart / 1000000LL);
 
-	return 0;
+    return 0;
 }
--- src/timer.h
+++ src/timer.h
@@ -3,13 +3,15 @@
     
     #define BILLION (1E9)
 
-	#ifdef	__cplusplus
-	extern "C" {
-	#endif
-		int _clock_gettime(int dummy, struct timespec *ct);
-		int _gettimeofday(struct timeval *p, void *tz);
-	#ifdef	__cplusplus
-	}
-	#endif
+    #ifdef    __cplusplus
+    extern "C" {
+    #endif
 	
+    int _clock_gettime(int dummy, struct timespec *ct);
+    int _gettimeofday(struct timeval *p, void *tz);
+	
+    #ifdef    __cplusplus
+    }
+    #endif
+    
 #endif
